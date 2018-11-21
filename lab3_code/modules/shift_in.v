0ae69aa7679f70559731e5c553108ffd620309fb
Roni Peled <roni@mediajel.com>
2018-11-20 22:45:14 -0500

    Fixed combinational logic

--- lab3_code/modules/shift_in.v
+++ lab3_code/modules/shift_in.v
@@ -11,23 +11,27 @@ reg local_fx;
 assign fx = local_fx;
 
 reg started;
+reg in_progress;
 reg start;
 reg [3:0] count;
 
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         started <= 'b0;
+        in_progress <= 'b0;
         local_x_parallel <= 'b0;
         local_fx <= 0;
         count <= 4'b1111;
     end else begin
-        if (start && (count != 'b0)) begin
+        if (start & ~started) begin
             started <= 'b1;
+            in_progress <= 'b0;
             local_x_parallel <= 'b0;
             local_fx <= 0;
             count <= 'b0;
-        end else if (started) begin
-            started <= 'b1;
+        end else if (started | in_progress) begin
+            started <= 'b0;
+            in_progress <= 'b1;
             if (count > 4'd11) begin
                 local_x_parallel <= local_x_parallel;
                 local_fx <= 'b1;
@@ -46,9 +50,11 @@ always @(posedge clk or posedge reset) begin
     end
 end
 
-always @(sx) begin
-    if (sx) begin
-        start = 'b1;
+always @(posedge sx or posedge reset) begin
+    if (reset) begin
+        start <= 'b0;
+    end else begin
+        start <= 'b1;
     end
 end
 
